ca97f4180cd775714fb6914a96760803a8b4f680
JaehongDev <println@kakao.com>
2022-12-29 20:06:05 +0900

    refactor (AuthService) : 회원가입이후 응답 dto를 내려주도록 변경

--- src/main/java/com/example/jwtspringboot3example/service/AuthService.java
+++ src/main/java/com/example/jwtspringboot3example/service/AuthService.java
@@ -1,7 +1,7 @@
 package com.example.jwtspringboot3example.service;
 
-import com.example.jwtspringboot3example.entity.Users;
 import com.example.jwtspringboot3example.payload.request.SignupUserDto;
+import com.example.jwtspringboot3example.payload.response.SignupResponseDto;
 import com.example.jwtspringboot3example.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final AuthRepository authRepository;
 
-    public void signup(final SignupUserDto signupUserDto){
-        var savedUser = this.authRepository.save(signupUserDto.toEntity());
+    public SignupResponseDto signup(final SignupUserDto signupUserDto){
+        return new SignupResponseDto(this.authRepository.save(signupUserDto.toEntity()));
     }
 
 }
