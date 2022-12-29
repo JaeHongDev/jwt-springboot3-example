c9a4eddcc801922a357e376c8c230a714c129d9a
JaehongDev <println@kakao.com>
2022-12-29 20:06:34 +0900

    test (AuthServiceTest) : 정상적인 회원가입 로직에 대한 테스트코드 작성

---
+++ src/test/java/com/example/jwtspringboot3example/service/AuthServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.jwtspringboot3example.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.example.jwtspringboot3example.payload.request.SignupUserDto;
+import com.example.jwtspringboot3example.repository.AuthRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class AuthServiceTest {
+    @InjectMocks
+    private AuthService authService;
+    @Mock
+    private AuthRepository authRepository;
+
+
+    @Test
+    void 사용자가_생성되면_id_가_담겨져있는_dto를_반환한다() {
+        given(authRepository.save(any())).willReturn(1L);
+        var signupDto = SignupUserDto.builder()
+                .email("email")
+                .name("name")
+                .password("password")
+                .build();
+
+        var actual = authService.signup(signupDto).id();
+        var expected = 1L;
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
