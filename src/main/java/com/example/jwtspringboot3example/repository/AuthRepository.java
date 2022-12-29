a7c29e270c006bb91cf278661301eb99d086b105
JaehongDev <println@kakao.com>
2022-12-29 19:51:31 +0900

    feature (AuthRepository) : declare method that find Users by id

--- src/main/java/com/example/jwtspringboot3example/repository/AuthRepository.java
+++ src/main/java/com/example/jwtspringboot3example/repository/AuthRepository.java
@@ -1,9 +1,11 @@
 package com.example.jwtspringboot3example.repository;
 
 import com.example.jwtspringboot3example.entity.Users;
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthRepository {
-    Users save(Users users);
+    Long save(Users users);
+    Optional<Users> findById(Long userId);
 }
