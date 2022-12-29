fc2d381b85158db9037833cb25170abcd91822e7
JaehongDev <println@kakao.com>
2022-12-29 19:52:05 +0900

    feature (AuthRepositoryImpl) : implement findById method

--- src/main/java/com/example/jwtspringboot3example/repository/AuthRepositoryImpl.java
+++ src/main/java/com/example/jwtspringboot3example/repository/AuthRepositoryImpl.java
@@ -3,14 +3,20 @@ package com.example.jwtspringboot3example.repository;
 import com.example.jwtspringboot3example.entity.Users;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Optional;
 
 
 public class AuthRepositoryImpl implements AuthRepository{
     private long id = 1L;
     private final Map<Long, Users> usersMap = new LinkedHashMap<>();
     @Override
-    public Users save(Users user) {
-        this.usersMap.put(id++, user);
-        return user;
+    public Long save(Users user) {
+        this.usersMap.put(id, user);
+        return id;
+    }
+
+    @Override
+    public Optional<Users> findById(Long userId) {
+        return Optional.ofNullable(this.usersMap.get(userId));
     }
 }
